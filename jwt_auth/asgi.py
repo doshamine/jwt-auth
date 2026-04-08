1fa85db6527f98c1f66c0f5d568c82d617ba076b
doshamine <doshamine@gmail.com>
2026-04-09 00:24:21 +0600

    fix: fix project name

--- jwt_auth/asgi.py
+++ jwt_auth/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for effective_mobile_task project.
+ASGI config for jwt_auth project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "effective_mobile_task.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jwt_auth.settings")
 
 application = get_asgi_application()
--- jwt_auth/wsgi.py
+++ jwt_auth/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for effective_mobile_task project.
+WSGI config for jwt_auth project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "effective_mobile_task.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jwt_auth.settings")
 
 application = get_wsgi_application()
