fec232aafcdffc90bf7799ee64c75a35f62db305
doshamine <doshamine@gmail.com>
2026-04-25 11:04:51 +0600

    feat: created test environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,18 @@ from model_bakery import baker
 @pytest.fixture
 def role():
     return baker.make("user_auth.Role", _fill_optional=True)
+
+
+@pytest.fixture
+def permission():
+    return baker.make("user_auth.Permission", _fill_optional=True)
+
+
+@pytest.fixture
+def user():
+    return baker.make("user_auth.User", _fill_optional=True)
+
+
+@pytest.fixture
+def refresh_token():
+    return baker.make("user_auth.RefreshToken", _fill_optional=True)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,2 +1,13 @@
 import pytest
 
+from user_auth.models import Permission
+
+
+@pytest.mark.django_db
+def test_create_permission():
+    prepared_permission = Permission(name="test", description="test description")
+
+    prepared_permission.save()
+    saved_permission = Permission.objects.all().first()
+
+    assert prepared_permission == saved_permission
