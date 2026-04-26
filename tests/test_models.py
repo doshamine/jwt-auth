3aaa48e201f6869dcbf68725f6d233a39aec9801
doshamine <doshamine@gmail.com>
2026-04-26 12:56:33 +0600

    feat: added tests for permission create checks

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,6 @@
 import pytest
+from django.db import DataError, IntegrityError
+from model_bakery import baker
 
 from user_auth.models import Permission
 
@@ -11,3 +13,29 @@ def test_create_permission():
     saved_permission = Permission.objects.all().first()
 
     assert prepared_permission == saved_permission
+
+
+@pytest.mark.django_db
+def test_create_permission_large_length_error():
+    permission = Permission(name="test" * 100, description="test description")
+
+    with pytest.raises(DataError):
+        permission.save()
+
+
+@pytest.mark.django_db
+def test_create_permission_duplicate_error():
+    permission1 = Permission(name="test", description="test description")
+    permission1.save()
+    permission2 = Permission(name="test", description="description")
+
+    with pytest.raises(IntegrityError):
+        permission2.save()
+
+
+@pytest.mark.django_db
+def test_create_permission_null_error():
+    permission = Permission(name=None, description="test description")
+
+    with pytest.raises(IntegrityError):
+        permission.save()
